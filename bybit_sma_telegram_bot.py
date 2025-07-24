9f8a7c17305ea16a55d835dbb18e22fad19e4c3a
abrahamfami <frkn.pasa@gmail.com>
2025-07-24 11:42:53 +0300

    Update bybit_sma_telegram_bot.py

--- bybit_sma_telegram_bot.py
+++ bybit_sma_telegram_bot.py
@@ -12,7 +12,7 @@ TELEGRAM_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
 TELEGRAM_CHAT_ID = os.environ.get("TELEGRAM_CHAT_ID")
 
 symbol = "SUIUSDT"
-qty = 10  # Pozisyon miktarı
+qty = 30  # Pozisyon miktarı
 
 # Bybit API bağlantısı
 session = HTTP(testnet=False, api_key=BYBIT_API_KEY, api_secret=BYBIT_API_SECRET)
