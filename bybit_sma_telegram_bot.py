76ee9ac224d83c8d2994381b67adc84f89c70d05
abrahamfami <frkn.pasa@gmail.com>
2025-07-24 11:58:20 +0300

    Update bybit_sma_telegram_bot.py

--- bybit_sma_telegram_bot.py
+++ bybit_sma_telegram_bot.py
@@ -12,7 +12,7 @@ TELEGRAM_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
 TELEGRAM_CHAT_ID = os.environ.get("TELEGRAM_CHAT_ID")
 
 symbol = "SUIUSDT"
-qty = 10
+qty = 30
 interval = "5m"  # Binance'ten veri çekerken kullanılacak zaman aralığı
 
 # Bybit API bağlantısı
